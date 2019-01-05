f8721d576f66af8c2ffff3d5ff8e4ef96683f684
Marcos Del Sol Vives <marcos@orca.pet>
2019-01-05 15:46:39 +0100

    Project skeleton

---
+++ EmergencyStop/main.c
@@ -0,0 +1,18 @@
+/*
+ * EmergencyStop.c
+ *
+ * Created: 05/01/2019 15:39:28
+ * Author : Marcos
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
